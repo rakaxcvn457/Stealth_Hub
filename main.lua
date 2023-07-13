7d466aa5b9c838bb6aae56d93cb349b2642d7c96
rakaxcvn457 <98208019+rakaxcvn457@users.noreply.github.com>
2023-07-13 17:38:00 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,10 @@
+--[[
+
+Version: v1.0
+Supported games: Naval Warfare
+Discord server link: https://discord.gg/zTb8faUqQZ
+Recommended exploit: Fluxus
+
+]]--
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rakaxcvn457/Stealth_Hub/main/script/source.lua"))()
